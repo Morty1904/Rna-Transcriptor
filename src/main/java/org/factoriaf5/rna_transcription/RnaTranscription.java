3bdaa6758e2755269be38af7e7e4802e96014885
Morty1904 <oscarendercraft@gmail.com>
2024-11-25 10:20:49 +0100

    added program to transform dna nucleotides into rna nucleotides

--- src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -3,8 +3,31 @@ package org.factoriaf5.rna_transcription;
 
 public class RnaTranscription {
     
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
-    }
+    public String transcribe(String dnaStrand) {
+        if (dnaStrand==null || dnaStrand.isEmpty()){
+            return "";
+        }
+    
+    StringBuilder rnaStrand = new StringBuilder();
 
+    for(char nucleotide : dnaStrand.toCharArray()){
+        switch (nucleotide) {
+            case 'A':
+                rnaStrand.append('U');
+                break;
+            case 'G':
+                rnaStrand.append('C');
+                break;
+            case 'C':
+                rnaStrand.append('G');
+                break;
+            case 'T':
+                rnaStrand.append('A');
+                break;
+            default:
+            throw new IllegalArgumentException("Invalid nucleotide");
+        }
+    }
+    return rnaStrand.toString();
+    }
 }
